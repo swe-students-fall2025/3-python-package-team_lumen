d22733be9f424504dab6dc1bb624c5df83c6a280
Xuan4781 <Xuan4781@users.noreply.github.com>
2025-11-01 19:53:31 -0400

    add single letter isvalid test

--- tests/test_is_valid.py
+++ tests/test_is_valid.py
@@ -1,2 +1,13 @@
-def test_is_valid():
-    assert True
+import pytest
+from morseify.core import is_valid
+
+def test_valid_single():
+    assert is_valid("....") is True  
+    assert is_valid(".") is True
+    assert is_valid("-.--") is True
+    
+    assert is_valid("-...") is True
+    assert is_valid(".-.") is True
+    assert is_valid("---") is True
+
+
