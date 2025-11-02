18a4716e14626f0d9427de772c5175ffa0ab5d05
Xuan4781 <Xuan4781@users.noreply.github.com>
2025-11-01 20:00:59 -0400

    add multi letter isvalid test

--- tests/test_is_valid.py
+++ tests/test_is_valid.py
@@ -10,4 +10,8 @@ def test_valid_single():
     assert is_valid(".-.") is True
     assert is_valid("---") is True
 
-
+def test_valid_multiple():
+    morse1 = ".... . -.-- / -... .-. ---"           
+    morse2 = ".. / .-- .- -. - / .- / -.-. .- -"   
+    assert is_valid(morse1) is True
+    assert is_valid(morse2) is True
