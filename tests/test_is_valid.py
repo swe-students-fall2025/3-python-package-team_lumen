d09ed269520c39c65ea77628c84731def61ee351
Xuan4781 <123594280+Xuan4781@users.noreply.github.com>
2025-11-03 11:37:12 -0500

    add isvalid test for invalid char, empty and extra space inputs

--- tests/test_is_valid.py
+++ tests/test_is_valid.py
@@ -30,3 +30,16 @@ def test_valid_seperator():
     assert is_valid(morse1) is True
     assert is_valid(morse2) is True
 
+def test_invalid_morse_char():
+    assert is_valid("x") is False
+    assert is_valid(".... x ...") is False
+    assert is_valid(".... & ...") is False
+
+def test_emptywhite_space():
+    assert is_valid("") is False
+    assert is_valid("   ") is False
+    assert is_valid(None) is False
+
+def test_valid_morse_extraspace():
+    morse = ".... . .-.. .-.. ---   / .-- --- .-. .-.. -.."
+    assert is_valid(morse) is True
