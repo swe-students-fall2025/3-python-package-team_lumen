25d7aa486180865cf2fc253a945fe2916f1f31db
Xuan4781 <Xuan4781@users.noreply.github.com>
2025-11-04 18:29:44 -0500

    initial setup for example demo example

--- examples/demo.py
+++ examples/demo.py
@@ -1,1 +1,18 @@
+'''
+This demo will showcase the core functionalities:
+- Encoding
+- Decoding
+- Normalization
+- Validate
+- Explaining
+- Quiz
+'''
 
+from morseify import encode, decode, normalize_text, normalize_code, is_valid, explain, quiz
+
+def main():
+    print("Demo")
+
+
+if __name__ == "__main__":
+    main()
